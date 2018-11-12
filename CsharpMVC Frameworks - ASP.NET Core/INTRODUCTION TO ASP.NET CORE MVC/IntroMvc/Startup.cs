c359649c7341b69f20888c4b46dc2c83cef4b04f
warglaivee <warglaivegm@gmail.com>
2018-11-12 15:06:05 +0200

    refactor

--- CsharpMVC Frameworks - ASP.NET Core/INTRODUCTION TO ASP.NET CORE MVC/IntroMvc/Startup.cs
+++ CsharpMVC Frameworks - ASP.NET Core/INTRODUCTION TO ASP.NET CORE MVC/IntroMvc/Startup.cs
@@ -64,13 +64,6 @@ namespace IntroMvc
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "showCats",
-                    template: "/show/{name}",
-                defaults: new { controller = "Cat", action = "Show" });
-            });
         }
     }
 }
