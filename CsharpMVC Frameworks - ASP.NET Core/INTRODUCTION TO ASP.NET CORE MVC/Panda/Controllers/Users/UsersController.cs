7d7360d17f3095e269d7b8b651d72d6e21623b2e
warglaivee <warglaivegm@gmail.com>
2018-11-14 16:30:40 +0200

    home and login made.

---
+++ CsharpMVC Frameworks - ASP.NET Core/INTRODUCTION TO ASP.NET CORE MVC/Panda/Controllers/Users/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Panda.Controllers.Users
+{
+    public class UsersController : BaseController
+    {
+        [HttpGet("Users/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
